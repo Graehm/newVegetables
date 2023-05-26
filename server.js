9f14ec03e45c7a9955551fbc8bf22bca66d3d776
Graehm <gpatrellfazio@gmail.com>
2023-05-25 21:14:21 -0500

    web er

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const jsxEngine = require('jsx-view-engine')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.once('open', () => {
+    console.log('Youre connected bitchhhhhh')
+})
+
+app.use(express.urlencoded({ extended: true}))
+app.set('view engine','jsx')
+app.engine('jsx',jsxEngine())
+
+app.get('vegetables/new', (req, res) => {
+    res.send('/New')
+})
+
+app.listen(PORT, () => {
+    console.log(`You're listening on Port ${PORT}!`)
+})
---
+++ views/New.jsx
@@ -0,0 +1,4 @@
+const react = require('react')
+function New ()  {
+    return(<h1>You're Here</h1>)
+}
